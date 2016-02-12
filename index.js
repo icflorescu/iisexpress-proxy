ec1efd2cbfbd1ad60b1ee9f08be9e49ca66bce57
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2016-02-12 22:58:58 +0200

    Fix #3 - add support for proxying SSL servers to localhost

--- index.js
+++ index.js
@@ -2,51 +2,65 @@
 
 var os = require('os'),
     proxy = require('http-proxy'),
-    interfaces = os.networkInterfaces(),
-    pkg = require('./package'),
-    ver = pkg.version,
-    exit = function() {
-      console.log('Usage example:\n %s [192.168.0.100:]51123 to 3000', Object.keys(pkg.bin)[0]);
-      process.exit();
-    },
-    source, host, port, proxyPort, currentProxy;
+    pkg = require('./package');
 
-console.log('IIS Express Proxy %s', ver);
+var exit = function() {
+  var bin = Object.keys(pkg.bin)[0];
+  console.log('Usage examples:');
+  console.log('\t%s 51123 to 3000', bin);
+  console.log('\t%s [http(s)://]192.168.0.100:51123 to 3000', bin);
+  console.log('\t%s [http://]domain.com:80 to 3000', bin);
+  console.log('\t%s [https://]ssl-domain.com:443 to 3000', bin);
+  console.log();
+  process.exit();
+};
+
+console.log('IIS Express Proxy %s', pkg.version);
 
 if (process.argv.length != 5 || process.argv[3].toLowerCase() !== 'to') {
   exit();
 }
 
-source    = process.argv[2].split(':');
-host      = source.length == 1 ? 'localhost' : source[0];
-port      = parseInt(source[source.length == 1 ? 0 : 1]);
-proxyPort = parseInt(process.argv[4]);
+var source = process.argv[2].match(/^(https?:\/\/)?(.+?)(?:\:(\d+))$/);
+var protocolPrefix = 'http://',
+    host = 'localhost',
+    port, proxyPort;
+
+if (source === null) {
+  port = parseInt(process.argv[2], 10);
+} else {
+  protocolPrefix = source[1] || 'http://';
+  host = source[2];
+  port = parseInt(source[3], 10);
+}
+proxyPort = parseInt(process.argv[4], 10);
 
 if (isNaN(port) || isNaN(proxyPort)) {
   exit();
 }
 
-console.log('Proxying %s:%d to:', host, port);
+console.log('Proxying %s%s:%d to network interfaces:', protocolPrefix, host, port);
+
+var interfaces = os.networkInterfaces();
 
 Object.keys(interfaces).forEach(function(name) {
   interfaces[name].filter(function(item) {
     return item.family == 'IPv4' && !item.internal;
   }).forEach(function(item) {
-    console.log("- %s: %s:%s", name, item.address, proxyPort);
+    console.log("\t%s: %s:%s", name, item.address, proxyPort);
   });
 });
 
-currentProxy = proxy.createProxyServer({
-  target: 'http://' + host + ':' + port,
+proxy.createProxyServer({
+  target: protocolPrefix + host + ':' + port,
   changeOrigin: true
 }).listen(proxyPort, function() {
-  console.log('Listening... [ press Control-C to exit ]');
-});
-
-currentProxy.on('error', function (err, req, res) {
+  console.log('Listening... [press Control-C to exit]');
+}).on('error', function (err, req, res) {
+  console.log(err.stack);
+  console.log('Listening... [press Control-C to exit]');
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
-  console.log(err.stack);
-  res.end('Aw snap; something went wrong. Check your console to see the error.');
+  res.end('Aw snap! Something went wrong. Check your console to see the error.');
 });
